5d3b2e8db9f566a000f9636e08b0bebc178390a1
root <root@ip-30-10-2-243.ap-south-1.compute.internal>
2021-07-26 08:27:57 +0000

    ok1

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'http://15.206.168.0:3000/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
